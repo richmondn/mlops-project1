08e6272639bf694d2a286b646c9766b4868b74c9
richmondn <20654625+richmondn@users.noreply.github.com>
2024-07-19 17:37:22 -0500

    chore: Update Azure provider features in main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -8,7 +8,15 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = true
+      recover_soft_deleted_key_vaults = true
+    }
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
 
 data "azurerm_client_config" "current" {}
