94f0d404e90bf5c56956b39017c87b5eb08f5887
richmondn <20654625+richmondn@users.noreply.github.com>
2024-07-19 19:25:01 -0500

    chore: Add log analytics workspace for application insights

--- infrastructure/modules/application-insights/main.tf
+++ infrastructure/modules/application-insights/main.tf
@@ -6,3 +6,19 @@ resource "azurerm_application_insights" "appi" {
 
   tags = var.tags
 }
+
+resource "azurerm_log_analytics_workspace" "logwksp" {
+  name                = "logwksp-${var.prefix}-${var.postfix}${var.env}"
+  location            = var.location
+  resource_group_name = var.rg_name
+  sku                 = "Free"
+  retention_in_days   = 7
+}
+
+resource "azurerm_application_insights" "appi" {
+  name                = "appi-${var.prefix}-${var.postfix}${var.env}"
+  location            = var.location
+  resource_group_name = var.rg_name
+  workspace_id        = azurerm_log_analytics_workspace.logwksp.id
+  application_type    = "web"
+}
--- infrastructure/modules/application-insights/outputs.tf
+++ infrastructure/modules/application-insights/outputs.tf
@@ -1,3 +1,11 @@
 output "id" {
   value = azurerm_application_insights.appi.id
 }
+
+output "instrumentation_key" {
+  value = azurerm_application_insights.appi.instrumentation_key
+}
+
+output "app_id" {
+  value = azurerm_application_insights.appi.app_id
+}
