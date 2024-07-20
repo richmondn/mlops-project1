9b878c7fd80a445163241216d6625eefa94e7b60
richmondn <20654625+richmondn@users.noreply.github.com>
2024-07-19 19:32:00 -0500

    chore: Update log analytics workspace configuration

--- infrastructure/modules/application-insights/main.tf
+++ infrastructure/modules/application-insights/main.tf
@@ -1,18 +1,11 @@
-resource "azurerm_application_insights" "appi" {
-  name                = "appi-${var.prefix}-${var.postfix}${var.env}"
-  location            = var.location
-  resource_group_name = var.rg_name
-  application_type    = "web"
-
-  tags = var.tags
-}
-
 resource "azurerm_log_analytics_workspace" "logwksp" {
   name                = "logwksp-${var.prefix}-${var.postfix}${var.env}"
   location            = var.location
   resource_group_name = var.rg_name
   sku                 = "Free"
   retention_in_days   = 7
+
+  tags = var.tags
 }
 
 resource "azurerm_application_insights" "appi" {
@@ -21,4 +14,6 @@ resource "azurerm_application_insights" "appi" {
   resource_group_name = var.rg_name
   workspace_id        = azurerm_log_analytics_workspace.logwksp.id
   application_type    = "web"
+
+  tags = var.tags
 }
